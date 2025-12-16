8a4f6df99c5be5b8d48beaad5aa050c014fe2fd6
Nandalal Shukla <shuklanandalal@gmail.com>
2025-12-16 19:32:58 +0530

    created the initail folder structure

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-);
